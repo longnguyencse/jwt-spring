b78b520fe52383374acbee11f6d8af292d27e32f
Longbkit <longnguyenqu@gmail.com>
2018-05-28 17:30:14 +0700

    first commit

---
+++ src/main/java/kalog/com/vn/learn/config/WebSecurityConfig.java
@@ -0,0 +1,9 @@
+package kalog.com.vn.learn.config;
+
+/**
+ * Author Mikorn vietnam
+ * Created on 28-May-18.
+ */
+
+public class WebSecurityConfig {
+}
---
+++ src/main/java/kalog/com/vn/learn/controller/route/Route.java
@@ -0,0 +1,9 @@
+package kalog.com.vn.learn.controller.route;
+
+/**
+ * Author Mikorn vietnam
+ * Created on 28-May-18.
+ */
+
+public class Route {
+}
